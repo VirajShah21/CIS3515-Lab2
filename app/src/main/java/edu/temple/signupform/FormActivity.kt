04482a041954e5a6b079f6cecf3d6c095c55e860
Viraj Shah <shahvirajd@gmail.com>
2021-09-23 04:54:59 -0400

    Adapted form response to be within the `EditText`.

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 class FormActivity : AppCompatActivity() {
@@ -19,24 +20,14 @@ class FormActivity : AppCompatActivity() {
             val passwordConf = findViewById<EditText>(R.id.passwordConf);
 
             if (fullname.text.isEmpty() || email.text.isEmpty() || password.text.isEmpty() || passwordConf.text.isEmpty()) {
-                // build alert dialog
-                val dialogBuilder = AlertDialog.Builder(this)
+                if (fullname.text.isEmpty())
+                    fullname.error = "Cannot be blank"
 
-                // set message of alert dialog
-                dialogBuilder.setMessage("All fields must be filled!")
-                    // if the dialog is cancelable
-                    .setCancelable(true)
-                    // negative button text and action
-                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
-                        dialog.cancel()
-                    })
+                if (email.text.isEmpty()) email.error = "Cannot be blank";
 
-                // create dialog box
-                val alert = dialogBuilder.create()
-                // set title for alert dialog box
-                alert.setTitle("AlertDialogExample")
-                // show alert dialog
-                alert.show()
+                if (password.text.isEmpty()) password.error = "Cannot be blank";
+
+                if (passwordConf.text.isEmpty()) passwordConf.error = "Cannot be blank";
             } else if (password.text.equals(passwordConf.text)) {
                 // build alert dialog
                 val dialogBuilder = AlertDialog.Builder(this)
@@ -56,25 +47,11 @@ class FormActivity : AppCompatActivity() {
                 alert.setTitle("AlertDialogExample")
                 // show alert dialog
                 alert.show()
+
+                password.error = "Passwords do not match"
+                passwordConf.error = "Passwords do not match"
             } else {
-                // build alert dialog
-                val dialogBuilder = AlertDialog.Builder(this)
-
-                // set message of alert dialog
-                dialogBuilder.setMessage("Hello " + fullname.text)
-                    // if the dialog is cancelable
-                    .setCancelable(true)
-                    // negative button text and action
-                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
-                        dialog.cancel()
-                    })
-
-                // create dialog box
-                val alert = dialogBuilder.create()
-                // set title for alert dialog box
-                alert.setTitle("AlertDialogExample")
-                // show alert dialog
-                alert.show()
+                Toast.makeText(getApplicationContext(), fullname.text, Toast.LENGTH_SHORT).show()
             }
         }
     }
